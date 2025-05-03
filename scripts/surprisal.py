fb8eef80e83e36b68cbd5ddb60f51d9cfea9e1f5
hilalcomak <hilal.comak@uzh.ch>
2025-05-03 18:51:40 +0200

    Add paper equations for surprisal.

--- scripts/surprisal.py
+++ scripts/surprisal.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
 import argparse
+import numpy as np
+from tqdm import tqdm
 import sys
 from metrics import translator
+
 sys.path.append('probability-of-a-word')
 from wordsprobability import get_surprisal_per_word
 import wordsprobability.models as wp_models
@@ -10,10 +13,25 @@ import wordsprobability.models as wp_models
 
 def main(args):
   assert args.lang in wp_models.LANGUAGES[args.model], f"Language {args.lang} not supported by model {args.model}"
-  content = args.src.read()
-  content = content.translate(translator)
-  df = get_surprisal_per_word(content, args.model)
-  print(df)
+  measures = {k: list() for k in set(args.measure)}
+
+  sentences = args.src.readlines()
+  sentences = [s.translate(translator).strip() for s  in sentences]
+  sentences = [s for s in sentences if s != ""]
+  for sent in tqdm(sentences):
+    s = get_surprisal_per_word(sent, args.model)['surprisal'].to_numpy()
+    #https://arxiv.org/pdf/2306.03734
+    if 'uid_variance' in measures:
+      # Eq 1
+      μ = np.mean(s)
+      measures['uid_variance'].append(np.mean(np.square(s - μ)))
+    # https://arxiv.org/pdf/2306.03734
+    if 'uid_w2w_change' in measures:
+      if s.size > 1:
+        # Eq 2
+        measures['uid_w2w_change'].append(np.sum(np.square(s[1:]-s[:-1]))/(s.size-1))
+  for k,v in measures.items():
+    print(f"{k}: {np.mean(v)}")
   exit(0)
 
 def valid_languages():
@@ -31,9 +49,12 @@ if __name__ == "__main__":
         epilog=example,
         formatter_class = argparse.RawDescriptionHelpFormatter
     )
-    parser.add_argument("src", type=argparse.FileType('r', encoding='UTF-8'), help="Source file")
     parser.add_argument("--model", type=str, required=True, choices=wp_models.MODELS.keys())
     parser.add_argument("--lang", type=str, required=True, choices=valid_languages())
-    #parser.add_argument("measure", type=str, choices=["k-tau", "cross-entropy", "perplexity", "fuzzy-reordering"])
+    parser.add_argument("src", type=argparse.FileType('r', encoding='UTF-8'), help="Source file")
+    parser.add_argument("measure",
+                        type=str,
+                        nargs='+',
+                        choices=["uid_variance", "uid_w2w_change"])
     args = parser.parse_args()
     main(args)
