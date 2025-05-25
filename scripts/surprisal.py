824d02b03a300f1fe9372e50b0ffa8d12c6edfc1
hilalcomak <hilal.comak@uzh.ch>
2025-05-25 15:56:06 +0200

    Add German LM (Leo) to surprisal calculation

--- scripts/surprisal.py
+++ scripts/surprisal.py
@@ -5,56 +5,85 @@ import numpy as np
 from tqdm import tqdm
 import sys
 from metrics import translator
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
 
-sys.path.append('probability-of-a-word')
-from wordsprobability import get_surprisal_per_word
-import wordsprobability.models as wp_models
+MODELS = ["LeoLM/leo-hessianai-7b"]
 
+def get_model_and_tokenizer(args):
+  model = AutoModelForCausalLM.from_pretrained(args.model,
+                                            device_map='auto',
+                                            load_in_4bit=True,
+                                            torch_dtype=torch.bfloat16)
+  tokenizer = AutoTokenizer.from_pretrained(args.model)
+  tokenizer.pad_token = tokenizer.eos_token
+  model.config.pad_token_id = model.config.eos_token_id
+  return model, tokenizer
+
+#Based on https://discuss.huggingface.co/t/announcement-generation-get-probabilities-for-generated-output/30075/17
+def get_surprisal_per_token(model, tokenizer, input_texts):
+  input_ids = tokenizer(input_texts, padding=True, return_tensors="pt").input_ids
+  outputs = model(input_ids)
+  log_probs = torch.log_softmax(outputs.logits, dim=-1).detach()
+  # collect the probability of the generated token -- probability at index 0 corresponds to the token at index 1
+  log_probs = log_probs[:, :-1, :]
+  input_ids = input_ids[:, 1:]
+  gen_log_probs = torch.gather(log_probs, 2, input_ids[:, :, None]).squeeze(-1)
+  return -gen_log_probs
+
+def log_prob_variance(s):
+  # https://arxiv.org/pdf/2306.03734 Eq1
+  if s.size < 2:
+    return None
+  μ = np.mean(s)
+  return np.mean(np.square(s - μ))
+
+def log_prob_w2w_change(s):
+  # https://arxiv.org/pdf/2306.03734 Eq2
+  if s.size < 2:
+    return None
+  return np.sum(np.square(s[1:] - s[:-1])) / (s.size - 1)
+
+def log_prob_mean(s):
+  return np.mean(s)
+
+MEASURES = {
+  'log_prob_variance' : log_prob_variance,
+  'log_prob_w2w_change' : log_prob_w2w_change,
+  'log_prob_mean': log_prob_mean,
+}
 
 def main(args):
-  assert args.lang in wp_models.LANGUAGES[args.model], f"Language {args.lang} not supported by model {args.model}"
   measures = {k: list() for k in set(args.measure)}
-
   sentences = args.src.readlines()
   sentences = [s.translate(translator).strip() for s  in sentences]
   sentences = [s for s in sentences if s != ""]
+  model, tokenizer = get_model_and_tokenizer(args)
   for sent in tqdm(sentences):
-    s = get_surprisal_per_word(sent, args.model)['surprisal'].to_numpy()
-    #https://arxiv.org/pdf/2306.03734
-    if 'uid_variance' in measures:
-      # Eq 1
-      μ = np.mean(s)
-      measures['uid_variance'].append(np.mean(np.square(s - μ)))
-    # https://arxiv.org/pdf/2306.03734
-    if 'uid_w2w_change' in measures:
-      if s.size > 1:
-        # Eq 2
-        measures['uid_w2w_change'].append(np.sum(np.square(s[1:]-s[:-1]))/(s.size-1))
+    s = get_surprisal_per_token(model, tokenizer, [sent])[0].float().cpu().numpy()
+    #print(f"{sent}")
+    for k in measures:
+      r = MEASURES[k](s)
+      if r is not None:
+        #print(f"\t{k}: {r}")
+        measures[k].append(r)
+
   for k,v in measures.items():
     print(f"{k}: {np.mean(v)}")
   exit(0)
 
-def valid_languages():
-  ret = set()
-  for v in wp_models.LANGUAGES.values():
-    ret = ret | v
-  return ret
-
 example = """example:
-  python %(prog)s src.txt ref.txt k-tau
+  python %(prog)s src.txt --model LeoLM/leo-hessianai-7b out/flores/en-de/ref.txt log_prob_mean
 """
 if __name__ == "__main__":
-    assert wp_models.MODELS.keys() == wp_models.LANGUAGES.keys(), "All models must have a language"
     parser = argparse.ArgumentParser(
         epilog=example,
         formatter_class = argparse.RawDescriptionHelpFormatter
     )
-    parser.add_argument("--model", type=str, required=True, choices=wp_models.MODELS.keys())
-    parser.add_argument("--lang", type=str, required=True, choices=valid_languages())
+    parser.add_argument("--model", type=str, required=True, choices=MODELS)
     parser.add_argument("src", type=argparse.FileType('r', encoding='UTF-8'), help="Source file")
     parser.add_argument("measure",
                         type=str,
                         nargs='+',
-                        choices=["uid_variance", "uid_w2w_change"])
-    args = parser.parse_args()
-    main(args)
+                        choices=MEASURES.keys())
+    main(parser.parse_args())
